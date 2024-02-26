f6411981cd5498155dd09c869daf88abf492e3b3
Joy Muthoni <kiboijoye254@gmail.com>
2024-02-26 23:41:32 +0300

    ADD:React JSX

---
+++ React Docs/my-app/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// const name = 'Joy Kiboi';
+// const element = <h1>Hello {name}</h1>
+function formatName(user){
+  return user.firstName + ' ' + user.lastName
+}
+
+const user={
+  firstName: 'Joy',
+  lastName: 'Kiboi'
+}
+
+const element = <h1>{formatName(user)}</h1>
+root.render(element);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
