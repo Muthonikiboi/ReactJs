8f414689afc7750f93f26bde04457da5394b3257
Joy Muthoni <kiboijoye254@gmail.com>
2024-03-18 22:10:46 +0300

    ADD:Props

--- React Docs/pract-app/src/index.js
+++ React Docs/pract-app/src/index.js
@@ -4,6 +4,7 @@ import "./App.css";
 import {Profile} from "./Gallery.js";
 import Gallery from "./Gallery.js";
 import { MyDetails } from './Gallery.js';
+import Persons from './props.js';
 
 export default function App(){
     return (
@@ -11,9 +12,13 @@ export default function App(){
         <Gallery/>
         <Profile/>
         <MyDetails/>
+        <Persons/>
         </>
     )
 }
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
