e273052f99f690783d4cb686839a8a2f1404e855
Muthonikiboi <kiboijoye254@gmail.com>
2024-10-28 09:57:48 +0300

    ADD:Product Page

--- Product Page/my-project/src/App.jsx
+++ Product Page/my-project/src/App.jsx
@@ -1,15 +1,23 @@
-
 import './App.css'
+import ShoeDetails from './components/shoeDetails'
 
 function App() {
-
   return (
-    <>
-    <div>
-      <h1 className='text-white-950'>This is the first Tailwind project</h1>
+    <div className=" h-screen w-screen bg-gray-300 flex justify-center items-center">
+        <div className=" bg-white h-1/2 w-1/4 flex justify-center items-center">
+          <img src="src/assets/2473264-47d689359f412b.webp" alt="" className='z-40 '/>
+          <div>
+          </div>
+        </div>
+        <ShoeDetails 
+            productPage={'Product Name'}
+            color={'COLOR'}
+            productTitle={'PRODUCT DESCRIPTION'}
+            productInfo={'More info:'} 
+            productSize={'SIZE'} 
+            productPrice={45.50}
+        />
     </div>
-    </>
   )
 }
-
 export default App
